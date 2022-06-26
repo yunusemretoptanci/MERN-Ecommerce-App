3e719d44b2763c7b7ad6ac3997605e2ade3b93ad
yunusemretoptanci <94935315+yunusemretoptanci@users.noreply.github.com>
2022-06-26 12:27:28 +0300

    fixed 404 after refresh

--- ecommerceapi/index.js
+++ ecommerceapi/index.js
@@ -21,7 +21,7 @@ app.use("/api/products", productRoute);
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join("./client/build")));
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
     })
 }
 app.listen(process.env.PORT||5000,()=>{
